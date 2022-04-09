49a42781e8f229a18e21c7bc72fed5e07c2b0ab5
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2022-04-09 20:51:29 +0300

    added HW5 and install react-router-dom

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Header from './Header'
-import Routes from './Routes'
+import RoutesAll from "./Routes";
+import {HashRouter} from "react-router-dom";
+
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter>
 
             <Header/>
 
-            <Routes/>
+            <RoutesAll/>
 
-            {/*</HashRouter>*/}
+            </HashRouter>
         </div>
     )
 }
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    const activeClassForLink = (isActive: boolean) => {
+        return s.link + (!isActive ? s.active : '');
+    }
+
+
+    return (
+        <div className={s.header}>
+            <NavLink to={PATH.PRE_JUNIOR} className={() => activeClassForLink(true)}>pre-junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={() => activeClassForLink(true)}>junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={() => activeClassForLink(true)}>junior-plus</NavLink>
+            <div className={s.block}></div>
         </div>
     )
 }
